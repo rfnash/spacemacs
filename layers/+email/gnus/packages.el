8c79c66574c16276991f7575390c15539e880ca0
Robert F. Nash <robert@robertnash.net>
2015-12-23 23:16:35 -0500

    initial commit of temp

--- layers/+email/gnus/packages.el
+++ layers/+email/gnus/packages.el
@@ -16,13 +16,42 @@
   "Initialize my package"
   (use-package gnus
     :defer t
-    :commands gnus
+    :bind ("C-c g" . rfnash-visit-gnus-buffer)
+    :commands (gnus rfnash-visit-gnus-buffer)
     :init
-    (evil-leader/set-key "ag" 'gnus)
+    (evil-leader/set-key
+      "ag" 'gnus
+      "og"  'rfnash-visit-gnus-buffer)
     :config
     (progn
-    ;; No primary server:
-    ;; (setq gnus-select-method '(nnnil ""))
+    (setq gnus-select-method '(nntp "news.gwene.org"))
+    (setq gnus-secondary-select-methods
+          '((nnimap "rfnash@openmailbox.org"
+                    (nnimap-address "imap.openmailbox.org")
+                    (nnimap-server-port 143)
+                    (nnimap-user "rfnash@openmailbox.org")
+                    (nnimap-stream starttls))
+            (nnimap "robertnash@openmailbox.org"
+                    (nnimap-address "imap.openmailbox.org")
+                    (nnimap-server-port 143)
+                    (nnimap-user "robertnash@openmailbox.org")
+                    (nnimap-stream starttls))
+            (nnimap "musicmaker1118@gmail.com"
+                    (nnimap-address "imap.gmail.com")
+                    (nnimap-user "musicmaker1118@gmail.com"))
+            (nnimap "localhost"
+                    (nnimap-stream ssl))
+            (nnimap "outlook"
+                    (nnimap-address "imap-mail.outlook.com")
+                    (nnimap-user "rfnash@outlook.com"))
+            (nnimap "robertnash.net"
+                    (nnimap-address "imap.gmail.com")
+                    (nnimap-user "robert@nash.co.vu"))))
+
+    (setq message-send-mail-function 'message-send-mail-with-sendmail
+          message-sendmail-f-is-evil 't ; this is needed to allow msmtp to do its magic
+          sendmail-program "msmtp"
+          message-sendmail-extra-arguments '("--read-envelope-from" "-t"))
 
     ; Use topics per default:
     (add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
@@ -41,14 +70,24 @@
         gnus-group-line-format "%M%S%p%P%5y:%B %G\n";;"%B%(%g%)"
         gnus-summary-thread-gathering-function 'gnus-gather-threads-by-references
         gnus-thread-sort-functions '(gnus-thread-sort-by-most-recent-date)
+       ;; Make Gnus NOT ignore [Gmail] mailboxes
         gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\”]\”[#’()]"
+       nnmail-expiry-wait 'immediate
+       gnus-group-default-list-level 3
+       gnus-save-duplicate-list t
+       gnus-suppress-duplicates t
+       gnus-view-pseudos 'not-confirm
+       user-full-name "Robert F. Nash"
+       user-mail-address "robert@robertnash.net"
+       gnus-buttonized-mime-types '("multipart/alternative")
+       ;; mm-discouraged-alternatives '("text/html" "text/richtext")
         gnus-sum-thread-tree-false-root ""
         gnus-sum-thread-tree-indent " "
         gnus-sum-thread-tree-leaf-with-other "├► "
         gnus-sum-thread-tree-root ""
         gnus-sum-thread-tree-single-leaf "╰► "
         gnus-sum-thread-tree-vertical "│"
-        gnus-article-browse-delete-temp nil
+        gnus-article-browse-delete-temp nil 
         gnus-fetch-old-headers t
         gnus-treat-strip-trailing-blank-lines 'last
         gnus-keep-backlog 'nil
@@ -58,6 +97,42 @@
         smiley-style 'medium
         gnus-keep-backlog '0)
 
+      (add-hook 'kill-emacs-hook 'gnus-group-exit)
+
+      (defun rfnash-visit-gnus-buffer ()
+        "Create or visit GNUS's group buffer."
+        (interactive)
+        (if (get-buffer "*Group*")
+            (rfnash-switch-buffer "*Group*")
+          (gnus)))
+
+      (defun my-fmt-width ()
+        "Find the width of the current window in the current font size."
+        (interactive)
+        (ceiling (/ (window-width) (expt text-scale-mode-step text-scale-mode-amount))))
+
+      ;; TODO: make interative, and allow entering a width,
+      ;; still defaulting to calculated one
+      (defun my-fmt ()
+        "Wash Gnus Articles by wrapping words to my liking."
+        (interactive)
+        (save-excursion
+          (gnus-article-fill-cited-article (my-fmt-width))
+          (gnus-article-strip-trailing-space)))
+
+      (define-key gnus-article-mode-map (kbd "\\") 'my-fmt)
+
+      (require 'wrap-to-fill)
+      (defun rfnash-gnus-article-mode-hook ()
+        "Increase font size and set wrap-to-fill-column-mode for Gnus Articles."
+        (interactive)
+        (text-scale-set 2)
+        (set-fill-column (floor (* (frame-width) 0.72 )))
+        (wrap-to-fill-column-mode t))
+
+      (add-hook 'gnus-article-mode-hook 'rfnash-gnus-article-mode-hook)
+      ;;(require 'mu4e-utils)
+      ;;(add-hook 'gnus-get-top-new-news-hook (lambda () (mu4e-update-mail-and-index nil)))
     (require 'browse-url)
     (require 'nnrss)
     (defun spacemacs/browse-nnrss-url (arg)
@@ -77,7 +152,25 @@
 
     (evilify gnus-group-mode gnus-group-mode-map
                (kbd "l") #'gnus-group-list-groups)
-    (evilify gnus-group-mode gnus-group-mode-map
-             (kbd "g") #'gnus-group-get-new-news)
     (evilify gnus-server-mode gnus-server-mode-map)
-    (evilify gnus-browse-mode gnus-browse-mode-map))))
+    (evilify gnus-browse-mode gnus-browse-mode-map)
+;;  (evilify gnus-article-mode gnus-article-mode-map)
+;;  (evilify gnus-summary-mode gnus-summary-mode-map
+;;      (kbd "J") 'gnus-summary-next-article
+;;      (kbd "K") 'gnus-summary-prev-article
+;;      (kbd "<RET>") 'spacemacs/browse-nnrss-url)
+))
+
+  ;; org-mime is initialized here because otherwise spacemacs
+  ;; complains that the org-mime package does not exist
+  (use-package org-mime
+    :defer t
+    :commands (org-mime-htmlize
+               org-mime-org-buffer-htmlize)
+    :init
+    (progn
+      ;; setup org-mime
+      (evil-leader/set-key-for-mode 'message-mode
+        "mo" 'org-mime-htmlize)
+      (evil-leader/set-key-for-mode 'org-mode
+        "mH" 'org-mime-org-buffer-htmlize))))
